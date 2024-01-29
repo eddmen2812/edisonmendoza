36bdad0872af4ad8a4a8c99a3987f4069f156220
Edison Mendoza <eypclark@hotmail.com>
2024-01-29 18:55:17 -0500

    first commit

---
+++ src/constants/urls.ts
@@ -0,0 +1,23 @@
+const GITHUB_URL = 'https://github.com/eddmen2812/';
+const LINKEDIN_URL = 'https://www.linkedin.com/in/deed2812';
+const TWITTER_URL = 'https://twitter.com/';
+const INSTAGRAM_URL = 'https://www.instagram.com/';
+const EMAIL_URL = 'edinson.mendoza@academia-ciberseguridad.com';
+const PUCE_URL = 'https://www.pucesa.edu.ec/mci/';
+const BADGE_CISCO_URL = 'https://www.credly.com/users/deed2812/';
+const ACAD_CIBER_URL = 'https://academia-ciberseguridad.com/en';
+const ACAD_CIBER_BADGE_URL = 'https://credential.certifyme.online/wallet/credentails/safe/184bc2b83453';
+const ACAD_HM_BADGE_URL = 'https://badgr.com/public/collections/5bb67bd49e6a4c17b36245dceec883e1'
+
+export {
+  GITHUB_URL,
+  LINKEDIN_URL,
+  TWITTER_URL,
+  INSTAGRAM_URL,
+  EMAIL_URL,
+  PUCE_URL,
+  BADGE_CISCO_URL,
+  ACAD_CIBER_URL,
+  ACAD_CIBER_BADGE_URL,
+  ACAD_HM_BADGE_URL,
+};
