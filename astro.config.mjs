eeae2ec27a382d506a6bde76e22a52e8762c1af6
Edison Mendoza <eypclark@hotmail.com>
2024-01-29 22:28:43 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
-  site: 'https://edisonmendoza.github.io',
-  base: '/edisonmendoza.github.io',
+  site: 'https://edisonmendoza',
+  base: '/edisonmendoza',
 });
