4f18d4cb661fdca5363eaebcc2c19dfaa796e57c
Edison Mendoza <eypclark@hotmail.com>
2024-01-31 11:54:13 -0500

    mi first commit

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -8,6 +8,7 @@ const BADGE_CISCO_URL = 'https://www.credly.com/users/deed2812/';
 const ACAD_CIBER_URL = 'https://academia-ciberseguridad.com/en';
 const ACAD_CIBER_BADGE_URL = 'https://credential.certifyme.online/wallet/credentails/safe/184bc2b83453';
 const ACAD_HM_BADGE_URL = 'https://badgr.com/public/collections/5bb67bd49e6a4c17b36245dceec883e1'
+const LPI01_CERT_URL = 'https://cs.lpi.org/caf/Xamman/certification/verify/LPI000561668/cyzc99dy84';
 
 export {
   GITHUB_URL,
@@ -20,4 +21,5 @@ export {
   ACAD_CIBER_URL,
   ACAD_CIBER_BADGE_URL,
   ACAD_HM_BADGE_URL,
+  LPI01_CERT_URL,
 };
