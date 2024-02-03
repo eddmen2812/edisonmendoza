32bf31fb535de8d567bea57eeb5563e6cd7bbbf9
Edison Mendoza <eypclark@hotmail.com>
2024-02-03 13:26:56 -0500

    Update commint

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -9,6 +9,7 @@ const ACAD_CIBER_URL = 'https://academia-ciberseguridad.com/en';
 const ACAD_CIBER_BADGE_URL = 'https://credential.certifyme.online/wallet/credentails/safe/184bc2b83453';
 const ACAD_HM_BADGE_URL = 'https://badgr.com/public/collections/5bb67bd49e6a4c17b36245dceec883e1'
 const LPI01_CERT_URL = 'https://cs.lpi.org/caf/Xamman/certification/verify/LPI000561668/cyzc99dy84';
+const TRYHACK_PROFILE_URL= 'https://tryhackme.com/p/deed';
 
 export {
   GITHUB_URL,
@@ -22,4 +23,5 @@ export {
   ACAD_CIBER_BADGE_URL,
   ACAD_HM_BADGE_URL,
   LPI01_CERT_URL,
+  TRYHACK_PROFILE_URL,
 };
