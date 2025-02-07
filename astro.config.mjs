1aa81bb31a32eefa94fa287d4bce4f19018ea25c
Edison Mendoza <eypclark@hotmail.com>
2025-02-07 13:58:48 -0500

    Update commint

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import svelte from '@astrojs/svelte';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), react(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ src/constants/urls.ts
@@ -0,0 +1,27 @@
+const GITHUB_URL = 'https://github.com/eddmen2812/';
+const LINKEDIN_URL = 'https://www.linkedin.com/in/deed2812';
+const TWITTER_URL = 'https://twitter.com/';
+const INSTAGRAM_URL = 'https://www.instagram.com/';
+const EMAIL_URL = 'eypclark@hotmail.com';
+const PUCE_URL = 'https://www.pucesa.edu.ec/mci/';
+const BADGE_CISCO_URL = 'https://www.credly.com/users/deed2812/';
+const ACAD_CIBER_URL = 'https://academia-ciberseguridad.com/en';
+const ACAD_CIBER_BADGE_URL = 'https://credential.certifyme.online/wallet/credentails/safe/184bc2b83453';
+const ACAD_HM_BADGE_URL = 'https://badgr.com/public/collections/5bb67bd49e6a4c17b36245dceec883e1'
+const LPI01_CERT_URL = 'https://cs.lpi.org/caf/Xamman/certification/verify/LPI000561668/cyzc99dy84';
+const TRYHACK_PROFILE_URL= 'https://tryhackme.com/p/deed';
+
+export {
+  GITHUB_URL,
+  LINKEDIN_URL,
+  TWITTER_URL,
+  INSTAGRAM_URL,
+  EMAIL_URL,
+  PUCE_URL,
+  BADGE_CISCO_URL,
+  ACAD_CIBER_URL,
+  ACAD_CIBER_BADGE_URL,
+  ACAD_HM_BADGE_URL,
+  LPI01_CERT_URL,
+  TRYHACK_PROFILE_URL,
+};
